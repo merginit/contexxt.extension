d878be388d50dd975987881ac4784e4962cd11f8
Jonas <publik.froeller.jonas@gmx.at>
2026-02-10 03:51:19 +0100

    feat: add multiple icon sizes and configure them in the WXT manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'wxt';
-// @ts-expect-error: false positive
 import Icons from 'unplugin-icons/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
     permissions: ['contextMenus', 'storage', 'activeTab', 'scripting', 'downloads'],
+    icons: {
+      16: 'icon/16.png',
+      32: 'icon/32.png',
+      48: 'icon/48.png',
+      96: 'icon/96.png',
+      128: 'icon/128.png',
+    },
   },
   vite: () => ({
     plugins: [
